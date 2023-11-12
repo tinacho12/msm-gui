ba9d011494268cb0c0f3011659d64d8adc745db2
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-11-12 02:12:32 +0000

    Fixed update bug

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,22 @@
 class MoviesController < ApplicationController
 
+  def update
+    m_id = params.fetch("an_id")
+    matching_records = Movie.where({ :id => m_id })
+    the_movie = matching_records.at(0)
+
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+  
+    the_movie.save
+
+    redirect_to("/movies/#{the_movie.id}")
+  end
+
   def create
     # Retrieve user inputs from params
     # Create a record in the movie table
